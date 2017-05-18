e76ad074cb79b1f435681a781216b524b35b9714
Charles Kim <Charles Kim>
2017-05-17 21:00:35 -0700

    sgt general functions working properly

--- src/components/StudentList.js
+++ src/components/StudentList.js
@@ -8,8 +8,16 @@ export default class StudentList extends Component{
         this.state={
             name: props.name,
             course: props.course,
-            grade: props.grade
+            grade: props.grade,
+            editTracker: false
         }
+    }
+
+    handleEdit(){
+        this.setState({
+                editTracker: true
+            }
+        )
     }
 
     dataDel(id){
@@ -17,21 +25,41 @@ export default class StudentList extends Component{
     }
 
     dataUpdate(id){
-        database.ref('studentList').child(id).update();
+        const {name, course, grade}=this.state;
+        if (name && course && grade){
+            database.ref('studentList').child(id).update({
+                name,
+                course,
+                grade
+            });
+            this.setState({
+                editTracker: false
+            })
+        }
     }
 
     render(){
         const {studentId, name, course, grade} = this.props;
-        if ()
-        return(
-            <tr>
-                <td>{name}</td>
-                <td>{course}</td>
-                <td>{grade}</td>
-                <td><button onClick={()=>this.dataUpdate(studentId)}>Edit</button><button onClick={()=>this.dataDel(studentId)}>Delete</button></td>
-            </tr>
-        )
-
+        if (!this.state.editTracker){
+            return(
+                <tr>
+                    <td>{name}</td>
+                    <td>{course}</td>
+                    <td>{grade}</td>
+                    <td><button onClick={()=>this.handleEdit()}>Edit</button><button onClick={()=>this.dataDel(studentId)}>Delete</button></td>
+                </tr>
+            )
+        }
+        else{
+            return(
+                <tr>
+                    <td><input onChange={(event)=>this.setState({name: event.target.value})} value={this.state.name} type="text" className="form-control" name="studentName" placeholder="Student Name" /></td>
+                    <td><input onChange={(event)=>this.setState({course: event.target.value})} value={this.state.course} type="text" className="form-control" name="studentCourse" placeholder="Student Course" /></td>
+                    <td><input onChange={(event)=>this.setState({grade: event.target.value})} value={this.state.grade} type="text" className="form-control" name="studentGrade" placeholder="Student Grade" /></td>
+                    <td><button onClick={()=>this.dataUpdate(studentId)}>Save</button><button onClick={()=>this.dataDel(studentId)}>Delete</button></td>
+                </tr>
+            )
+        }
     }
 }
 
